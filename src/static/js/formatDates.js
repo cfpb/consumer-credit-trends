e788ce5afc49fc74dbf351fd8da5422e5655bb8c
Catherine Farman <Catherine.Farman@cfpb.gov>
2016-11-30 01:09:18 -0500

    fix date formatting to output month + year for data

--- src/static/js/formatDates.js
+++ src/static/js/formatDates.js
@@ -1,51 +1,66 @@
 'use strict';
 
-var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var START_YEAR = 2000;
+var END_YEAR = 2016;
+var MONTHS_LIST = [
+  'January', 
+  'February', 
+  'March', 
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
 
-var remainders = [0, 0.0833333333, 0.1666666667];
+function createYearsList() {
 
-// start and end years for the data
-var years = [];
-var testYear = [];
+  var years = [];
 
-for (var year = 2000; year <= 2016; year++) {
-    years.push(year);
-    testYear.push(new Date(year));
+  for (var year = START_YEAR; year <= END_YEAR; year++) {
+      years.push(year);
+  }
+
+  return years;
+
 }
-
-
-console.log(years);
-console.log(testYear);
 
 // 0 is January 2000
 
-function displayMonthText(monthIndex) {
+function parseDate(monthIndex) {
+  var month = '';
+  var years = createYearsList();
 
-  // 0 = january
-  // 1 = february
-  // 12 = january
-  // % 0 = january
-  // 12/12 = january
-  // 13/12 = february
-  var month = months[monthIndex / months.length];
+  if ( monthIndex % MONTHS_LIST.length === 0 ) {
+    // remainder is 0 and you have your month: january
+    month = MONTHS_LIST[0];
+  } else {
+    month = MONTHS_LIST[monthIndex % MONTHS_LIST.length];
+  }
   
-  // 0-11 = year 0 (less than 1)
-  // 12-23 = year 1 (BETWEEN 1 AND 2)
-  // 24-35 = year 2 (2 and 3) 
-  // round down
   var year = years[Math.trunc(monthIndex / 12)];
 
-  return month + ' ' + year;    
+  return month + ' ' + year;
 }
 
 console.log(
-    // january 2000
-    'month 0 is ' + displayMonthText(0) + 
-    // September 2000
-    ' and month 10 is ' + displayMonthText(10) + 
-    // april 2000
-    ' and month 15 is ' + displayMonthText(15) +
-    // january 2002
-    ' and month 24 is ' + displayMonthText(24) +
-    ' and month 100 is ' + displayMonthText(100)
-    );
+  // january 2000
+  'month 0 is ' + parseDate(0) + 
+  // September 2000
+  ' and month 10 is ' + parseDate(10) + 
+  // april 2001
+  ' and month 15 is ' + parseDate(15) +
+  // january 2002
+  ' and month 24 is ' + parseDate(24) +
+  ' and month 50 is ' + parseDate(50) +
+
+  // may 2008
+  ' and month 100 is ' + parseDate(100) + 
+
+  // september 2016
+  ' and month 200 is ' + parseDate(200)
+  );
