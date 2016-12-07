402333f2df650b681568e819c0c6b68de13bd855
Catherine Farman <Catherine.Farman@cfpb.gov>
2016-12-07 17:06:40 -0500

    add todo note for organizing charts by directory

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -44,6 +44,7 @@ gulp.task( 'copy:release', function() {
   return gulp.src( release.src )
     .pipe( plugins.changed( release.dest ) )
     .on( 'error', handleErrors )
+    // @todo: generate directory structure in handlebars task based on the data passed from charts.json
     // .pipe( plugins.rename( {
     //   dirname: 'auto-loans/origination-activity',
     //   extname: '.html'
