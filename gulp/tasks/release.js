2de01cff6be3633258ddac469792c5d96f053636
Catherine Farman <Catherine.Farman@cfpb.gov>
2016-12-08 23:05:29 -0500

    clean up commented out refactored code

--- gulp/tasks/release.js
+++ gulp/tasks/release.js
@@ -17,16 +17,7 @@ var release = config.copy.release;
 
 gulp.task( 'release:copyFiles', function() {
   return gulp.src( release.src )
-    // .pipe( plugins.changed( release.dest ) ) // this doesn't always notice changes!
-    // .pipe( inlineCss( {
-    //   preserveMediaQueries: true
-    // }) )
     .on( 'error', handleErrors )
-    // @todo: generate directory structure in handlebars task based on the data passed from charts.json
-    // .pipe( plugins.rename( {
-    //   dirname: 'auto-loans/origination-activity',
-    //   extname: '.html'
-    // } ) )
     .pipe( gulp.dest( release.dest ) )
     .pipe( browserSync.reload( {
       stream: true
