9b5157a46e6306162e3c675e0a90335d5ac16c2b
Catherine Farman <Catherine.Farman@cfpb.gov>
2016-12-11 23:23:31 -0500

    add borrower risk charts and css for chart colors

--- src/static/js/templates/charts.js
+++ src/static/js/templates/charts.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var charts = [{
+  // Auto loans: Lending Levels
   "title": "Number of loans originated",
   "chartType": "line",
   "market": "auto-loans",
@@ -19,7 +20,7 @@ var charts = [{
   "elementID": "figure-1a__volume"
 },
 {
-  "title": "Map",
+  "title": "",
   "chartType": "map",
   "market": "auto-loans",
   "reportType": "origination-activity",  
@@ -44,6 +45,102 @@ var charts = [{
   "figureID": "figure-1b",
   "source": "yoy_data_all_AUT.csv",
   "elementID": "auto-loan_yoy-volume"
-}]
+},
+{ // Borrower Risk Profiles: Credit score
+  "title": "Deep subprime (credit scores below 580)",
+  "chartType": "line",
+  "market": "auto-loans",
+  "reportType": "borrower-risk-profiles",  
+  "figureID": "figure-2a",
+  "source": "volume_data_Score_Level_AUT.csv",
+  "elementID": "figure-2a__deep-subprime",
+  // Only add a group property if the data file needs to be filtered!
+  "group": "Deep Subprime"
+},
+{
+  "title": "Subprime (credit scores 580 - 619)",
+  "chartType": "line",
+  "market": "auto-loans",
+  "reportType": "borrower-risk-profiles",  
+  "figureID": "figure-2a",
+  "source": "volume_data_Score_Level_AUT.csv",
+  "elementID": "figure-2a__subprime",
+  "group": "Subprime"
+},
+{
+  "title": "Near prime (credit scores 620 - 679)",
+  "chartType": "line",
+  "market": "auto-loans",
+  "reportType": "borrower-risk-profiles",  
+  "figureID": "figure-2a",
+  "source": "volume_data_Score_Level_AUT.csv",
+  "elementID": "figure-2a__near-prime",
+  "group": "Near Prime"
+},
+{
+  "title": "Prime (credit scores 680 - 719)",
+  "chartType": "line",
+  "market": "auto-loans",
+  "reportType": "borrower-risk-profiles",  
+  "figureID": "figure-2a",
+  "source": "volume_data_Score_Level_AUT.csv",
+  "elementID": "figure-2a__prime",
+  "group": "Prime"
+},
+{
+  "title": "Superprime (credit scores 720 and above)",
+  "chartType": "line",
+  "market": "auto-loans",
+  "reportType": "borrower-risk-profiles",  
+  "figureID": "figure-2a",
+  "source": "volume_data_Score_Level_AUT.csv",
+  "elementID": "figure-2a__superprime",
+  "group": "Superprime"
+},
+{ // Year over year changes
+  "title": "Deep subprime (credit scores below 580)",
+  "chartType": "bar",
+  "market": "auto-loans",
+  "reportType": "borrower-risk-profiles",  
+  "figureID": "figure-2b",
+  "source": "yoy_data_Score_Level_AUT.csv",
+  "elementID": "auto-loan_yoy-deep-subprime"
+},
+{
+  "title": "Subprime (credit scores 580 - 619)",
+  "chartType": "bar",
+  "market": "auto-loans",
+  "reportType": "borrower-risk-profiles",  
+  "figureID": "figure-2b",
+  "source": "yoy_data_Score_Level_AUT.csv",
+  "elementID": "auto-loan_yoy-subprime"
+},
+{
+  "title": "Near prime (credit scores 620 - 679)",
+  "chartType": "bar",
+  "market": "auto-loans",
+  "reportType": "borrower-risk-profiles",  
+  "figureID": "figure-2b",
+  "source": "yoy_data_Score_Level_AUT.csv",
+  "elementID": "auto-loan_yoy-near-prime"
+},
+{
+  "title": "Prime (credit scores 680 - 719)",
+  "chartType": "bar",
+  "market": "auto-loans",
+  "reportType": "borrower-risk-profiles",  
+  "figureID": "figure-2b",
+  "source": "yoy_data_Score_Level_AUT.csv",
+  "elementID": "auto-loan_yoy-prime"
+},
+{
+  "title": "Superprime (credit scores 720 and above)",
+  "chartType": "bar",
+  "market": "auto-loans",
+  "reportType": "borrower-risk-profiles",  
+  "figureID": "figure-2b",
+  "source": "yoy_data_Score_Level_AUT.csv",
+  "elementID": "auto-loan_yoy-superprime"
+},]
 
 module.exports = charts;
