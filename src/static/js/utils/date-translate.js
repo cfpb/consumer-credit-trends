724e0652c0b9acd89d03d89a887d478f9d62f2b6
Catherine Farman <Catherine.Farman@cfpb.gov>
2016-12-16 16:37:16 -0500

    lint charts.js + util js

--- src/static/js/utils/date-translate.js
+++ src/static/js/utils/date-translate.js
@@ -1,10 +1,17 @@
 'use strict';
 
-// If 0 = January 2000, then we will translate an int to a month and year!
-
-function translate( int ) {
-  var year = Math.floor( int / 12 ) + 2000;
-  var month = int % 12 + 1;
+/**
+ * Returns a string representing the year and month based on a given integer,
+  where month 0 starts at January 1st 2000, month 1 is February 1st 2000, etc.
+ *
+ * @param {Number} index - used to represent the month and year in the data
+  sets, e.g., 0
+ * @returns {String} date - formatted as Year-Month-Day, e.g., 2000-January-01
+ */
+function translate( index ) {
+  var year = Math.floor( index / 12 ) + 2000;
+  var month = index % 12;
+  month += 1;
   if ( month < 10 ) {
     month = '0' + month;
   }
--- src/static/js/utils/dom.js
+++ src/static/js/utils/dom.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
+'use strict';
 
-var d3 = require( 'd3' );
 var jsdom = require( 'jsdom' ).jsdom;
 var fs = require( 'fs' );
 var charts = require( '../templates/charts.js' );
-var getFilePath = require( '../utils/getFilePath.js' );
+var getFilePath = require( '../utils/get-file-path.js' );
 var LOCALHOST_PATH = 'http://localhost:3000/';
 
 // run for each chart.
@@ -13,15 +13,25 @@ for ( var i = 0; i < charts.length; i++ ) {
 
   jsdom.env( {
     url: LOCALHOST_PATH + getFilePath( chart ),
-    scripts: ["http://localhost:3000/static/js/main.min.js"],
-    done: function ( err, window ) {
+    scripts: [ 'http://localhost:3000/static/js/main.min.js' ],
+    done: function( err, window ) {
 
+      if ( err ) {
+        process.stdout.write( err );
+      }
+
+      /**
+       * Copies the dynamic, rendered HTML for each chart to a compiled static
+        HTML file.
+       *
+       */
       function getSVG() {
         var code = window.document.querySelector( 'html' ).outerHTML;
-        var filePath = window.location.href.split(/^http:\/\/localhost:3000\//)[1];
+        var filePath =
+          window.location.href.split( /^http:\/\/localhost:3000\// )[1];
         fs.writeFileSync( './' + filePath, code );
-        process.stdout.write( 'location is ' + window.location.href + '\n');
-        process.stdout.write( 'writing to: ' + filePath + '\n');
+        process.stdout.write( 'location is ' + window.location.href + '\n' );
+        process.stdout.write( 'writing to: ' + filePath + '\n' );
       }
 
       window.setTimeout( getSVG, 17000 );
