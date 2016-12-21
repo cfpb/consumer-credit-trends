4be87bba7e3106dea83f8eac9ac0c38545acbec9
Catherine Farman <Catherine.Farman@cfpb.gov>
2016-12-21 15:12:17 -0500

    fix formatting

--- src/static/js/bar.js
+++ src/static/js/bar.js
@@ -2,7 +2,7 @@
 
 var chartBuilder = require( 'cfpb-chart-builder' );
 var d3 = require( 'd3' );
-var charts = require( './templates/charts.js' ); 
+var charts = require( './templates/charts.js' );
 var dateTranslate = require( './utils/date-translate.js' );
 var strToNum = require( './utils/string-to-number.js' );
 var formatTime = d3.utcFormat( '%b %Y' );
@@ -21,7 +21,7 @@ function init() {
       chartInfo.dataUrl = DATA_FILE_PATH + chartInfo.source;
       makeDataIntoBarCharts( chartInfo );
     }
-  };
+  }
 
 }
 
@@ -39,7 +39,7 @@ function makeDataIntoBarCharts( chartInfo ) {
       margin: {
         top: 85, right: 20, bottom: 50, left: 75
       }
-    }
+    };
 
     if ( chartInfo.hasOwnProperty( 'group' ) ) {
       var dataGroups = separateBarDataGroups( rawData );
@@ -70,7 +70,7 @@ function getMaxMonth( rawData ) {
     if ( +rawData[x].month > maxMonth ) {
       maxMonth = +rawData[x].month;
     }
-  };
+  }
 
   return maxMonth;
 }
@@ -79,14 +79,14 @@ function getMaxMonth( rawData ) {
 function reformatBarData( rawData ) {
   var data = [];
 
-   for (var x = 0; x < rawData.length; x++ ) {
+  for ( var x = 0; x < rawData.length; x++ ) {
     var obj = {
       label: dateTranslate( rawData[x].month ),
-      amount: strToNum ( rawData[x].yoy ) * 100
-    }; 
+      amount: strToNum( rawData[x].yoy ) * 100
+    };
 
     // add data if it's 2009 or after
-    if ( parseTime( obj.label ) >= parseTime( '2009-01-01') ) {
+    if ( parseTime( obj.label ) >= parseTime( '2009-01-01' ) ) {
       data.push( obj );
     }
 
@@ -104,14 +104,14 @@ function addProjectedToBar( svg, options ) {
       foo = svg.chart.selectAll( 'rect' );
 
   foo.filter( function( d, i ) {
-      if ( i === foo.size() - 7 ) {
-        date = parseTime( d.label );
-      } else if ( i === foo.size() - 6 ) {
-        x = this.getAttribute( 'x' );
-      }
+    if ( i === foo.size() - 7 ) {
+      date = parseTime( d.label );
+    } else if ( i === foo.size() - 6 ) {
+      x = this.getAttribute( 'x' );
+    }
 
-      return i > foo.size() - 7;
-    } )
+    return i > foo.size() - 7;
+  } )
     .attr( 'class', 'bar__projected' );
 
   var line = svg.chart.append( 'line' )
@@ -119,35 +119,35 @@ function addProjectedToBar( svg, options ) {
         .attr( 'x2', x )
         .attr( 'y1', -30 )
         .attr( 'y2', height )
-        .classed( 'axis__projected', true);
+        .classed( 'axis__projected', true );
 
   svg.chart.append( 'text' )
-      .attr('x', x )
-      .attr('y', -60 )
+      .attr( 'x', x )
+      .attr( 'y', -60 )
       .attr( 'class', 'gray-text' )
       .style( 'text-anchor', 'end' )
       .text( 'Values after ' + formatTime( date ) );
-  
+
   svg.chart.append( 'text' )
       .attr( 'x', x )
       .attr( 'y', -40 )
       .attr( 'class', 'gray-text' )
       .style( 'text-anchor', 'end' )
-      .text( 'are projected' )
+      .text( 'are projected' );
 
 }
 
 function separateBarDataGroups( rawData ) {
-    var obj = {};
+  var obj = {};
 
-    for (var x = 0; x < rawData.length; x++ ) {
-      if ( !obj.hasOwnProperty( rawData[x].group ) ) {
-        obj[rawData[x].group] = [];
-      }
-      obj[rawData[x].group].push( rawData[x] );
+  for ( var x = 0; x < rawData.length; x++ ) {
+    if ( !obj.hasOwnProperty( rawData[x].group ) ) {
+      obj[rawData[x].group] = [];
     }
+    obj[rawData[x].group].push( rawData[x] );
+  }
 
-    return obj;
+  return obj;
 }
 
 
